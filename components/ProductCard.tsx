28f0f8c1832c43afda052205f9d2d4d898e296bb
Nazmul Haque <bpinazmul18@gmail.com>
2023-07-14 09:34:39 +0600

    FIX: Fix product card link.

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,11 +1,12 @@
 import { ProductIProps } from '@/models/Product';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const ProductCard = ({ product }: {product: ProductIProps}) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-       <div className="relative h-[192px]">
+       <Link href={`/products/${product.id}`} className="relative block h-[192px]">
             <Image
                 src={product.image}
                 alt={product.title}
@@ -15,10 +16,25 @@ const ProductCard = ({ product }: {product: ProductIProps}) => {
                 placeholder='empty'
                 priority
             />
-       </div>
+       </Link>
+
+       {/* <div className='flex flex-col justify-between min-h-[240px]'>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 line-clamp-2">{product.title}</div>
+          <p className="text-gray-700 text-base line-clamp-3">{product.description}</p>
+        </div>
+        <div className="px-6 py-4 flex items-center justify-between">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            ${product.price}
+          </span>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Add to Cart
+          </button>
+        </div>
+       </div> */}
         
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2 line-clamp-2">{product.title}</div>
+        <Link href={`/products/${product.id}`} className="font-bold text-xl mb-2 line-clamp-2">{product.title}</Link>
         <p className="text-gray-700 text-base line-clamp-3">{product.description}</p>
       </div>
       <div className="px-6 py-4 flex items-center justify-between">
