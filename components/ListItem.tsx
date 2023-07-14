bbfba130e0b361d91c961db6456b3dd7e9e5247f
Nazmul Haque <bpinazmul18@gmail.com>
2023-07-14 08:41:26 +0600

    FIX: Fix Navbar item link.

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -9,7 +9,7 @@ const ListItem = ({ children, navItemStyles, NavLink } : IListProps) => {
         <li>
           <Link
             href={NavLink}
-            className={`flex py-2 text-lg font-medium leading-normal px-2 lg:px-7 lg:inline-flex ${navItemStyles}`}
+            className={`flex py-2 text-lg font-medium leading-normal capitalize px-2 lg:px-7 lg:inline-flex ${navItemStyles}`}
           >
             {children}
           </Link>
