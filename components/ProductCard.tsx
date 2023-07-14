84ee8da291981c30527ae3b72f1b6d09d3707cb1
Nazmul Haque <bpinazmul18@gmail.com>
2023-07-14 16:13:12 +0600

    WORK: Working on add to cart on productCard.

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,9 +1,12 @@
+import useCart from '@/hooks/useCart';
 import { ProductIProps } from '@/models/Product';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { FaMinus, FaPlus } from 'react-icons/fa';
 
 const ProductCard = ({ product }: {product: ProductIProps}) => {
+  const {addToCart, isItemInCart, updateQuantity, getQuantity} = useCart()
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
        <Link href={`/products/${product.id}`} className="relative block h-[192px]">
@@ -41,12 +44,27 @@ const ProductCard = ({ product }: {product: ProductIProps}) => {
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
           ${product.price}
         </span>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        {
+          !isItemInCart(product) ? 
+          <button onClick={()=> addToCart(product)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Add to Cart
-        </button>
+        </button> : 
+          <div className="flex items-center">
+            <button className="text-gray-500 mr-2" onClick={() => updateQuantity(product, getQuantity(product) - 1)} >
+                <FaMinus />
+            </button>
+            <input type="number" className="w-12 text-center border border-gray-300 rounded" value={getQuantity(product)}
+                onChange={() => {}} />
+            <button className="text-gray-500 ml-2" onClick={() => updateQuantity(product, getQuantity(product) + 1)} >
+                <FaPlus />
+            </button>
+          </div> 
+        }
       </div>
     </div>
   );
 };
 
 export default ProductCard;
+
+
